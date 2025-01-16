827f985a57441848f87c703e87f3ec44d20c4f2d
DuckyLine <legofftimeo2000@gmail.com>
2025-01-16 12:29:37 +0100

    Leak en moins

--- src/attach.c
+++ src/attach.c
@@ -6,7 +6,7 @@
 /*   By: tle-goff <tle-goff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:17:19 by tle-goff          #+#    #+#             */
-/*   Updated: 2025/01/10 13:12:28 by tle-goff         ###   ########.fr       */
+/*   Updated: 2025/01/16 12:09:34 by tle-goff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,51 @@
 char	*attach_block_quote(t_head *head)
 {
 	char	*result;
+	char	*temp;
 	t_node	*node;
 	t_list	*lst;
 	int		i;
 
 	i = 0;
-	result = "\0";
+	result = ft_strdup("");
 	lst = head->head;
 	while (lst)
 	{
 		node = lst->content;
 		if (i != 0 && node->head == 1)
-			result = ft_strjoin((const char *)result, (const char *)" ");
+		{
+			temp = result;
+			result = ft_strjoin(temp, " ");
+			free(temp);
+		}
 		if (node->type == 0)
-			result = ft_strjoin((const char *)result, (const char *)"\"");
+		{
+			temp = result;
+			result = ft_strjoin(temp, "\"");
+			free(temp);
+		}
 		if (node->type == 1)
-			result = ft_strjoin((const char *)result, (const char *)"\'");
-		result = ft_strjoin((const char *)result, (const char *)node->content);
+		{
+			temp = result;
+			result = ft_strjoin(temp, "\'");
+			free(temp);
+		}
+		temp = result;
+		result = ft_strjoin(temp, node->content);
+		free(temp);
 		if (node->type == 0)
-			result = ft_strjoin((const char *)result, (const char *)"\"");
+		{
+			temp = result;
+			result = ft_strjoin(temp, "\"");
+			free(temp);
+		}
 		if (node->type == 1)
-			result = ft_strjoin((const char *)result, (const char *)"\'");
+		{
+			temp = result;
+			result = ft_strjoin(temp, "\'");
+			free(temp);
+		}
+
 		if (!lst->next)
 			break ;
 		lst = lst->next;
@@ -47,19 +71,26 @@ char	*attach_block_quote(t_head *head)
 char	*attach_block(t_head *head)
 {
 	char	*result;
+	char	*temp;
 	t_node	*node;
 	t_list	*lst;
 	int		i;
 
 	i = 0;
-	result = "\0";
+	result = ft_strdup("");
 	lst = head->head;
 	while (lst)
 	{
 		node = lst->content;
 		if (i != 0 && node->head == 1)
-			result = ft_strjoin((const char *)result, (const char *)" ");
-		result = ft_strjoin((const char *)result, (const char *)node->content);
+		{
+			temp = result;
+			result = ft_strjoin(temp, " ");
+			free(temp);
+		}
+		temp = result;
+		result = ft_strjoin(temp, node->content);
+		free(temp);
 		if (!lst->next)
 			break ;
 		lst = lst->next;
